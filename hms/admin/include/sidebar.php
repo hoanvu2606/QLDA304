1ad242ddbe18710355b128daccb126f0ea76be83
unknown <hoanvu2661@gmail.com>
2022-12-28 13:34:47 +0700

    update

---
+++ hms/admin/include/sidebar.php
@@ -0,0 +1,123 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> TỔNG QUAN </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> BÁC SĨ </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Khoa khám </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Thêm bác sĩ</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> Quản lý bác sĩ </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> BỆNH NHÂN </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Quản lý bệnh nhân </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Danh sách lịch khám </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+	<li>
+								<a href="doctor-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Lịch sử Bác sĩ </span>
+										</div>
+									</div>
+								</a>
+							</li>		
+
+
+
+							<li>
+								<a href="user-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Lịch sử Bệnh nhân </span>
+										</div>
+									</div>
+								</a>
+							</li>						
+				
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
